0947a48c585f2167d12b2056fd960c8bf6b3ee1d
Troschenko Nikita <l0.trafalgar@yandex.ru>
2023-04-19 11:58:29 +0500

    Домашнее задание №5

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.open_url('https://demoqa.com/automation-practice-form')
-    browser.driver.set_window_size(1980, 1080)
-    #browser.execute_script("setTimeout(function(){debugger;}, 5000)")
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    browser.config.base_url = 'https://demoqa.com'
 
-
+    # browser.driver.set_window_size(1980, 1080)
+    # browser.execute_script("setTimeout(function(){debugger;}, 5000)")
--- test_entry_form.py
+++ test_entry_form.py
@@ -1,35 +1,33 @@
 import pytest
-from selene import browser, have, be
+from selene import browser, have
 import os
 
 
 def test_entry_form(browser_management):
+    browser.open('/automation-practice-form')
     browser.element('[class=main-header]').should(have.text('Practice Form'))
     browser.element('#firstName').type('Nikita')
     browser.element('#lastName').type('Troshenko')
     browser.element('#userEmail').type('gferrerewrrn@mail.ru')
     browser.element('.custom-control [for=gender-radio-1]').click()
-    browser.element('#userNumber').type('79767658977')
+    browser.element('#userNumber').type('9638460094')
     browser.element('#dateOfBirthInput').press()
     browser.element('.react-datepicker__month-select').click()
     browser.element('.react-datepicker__month-select').element('[value="5"]').click()
     browser.element('.react-datepicker__year-select').click()
     browser.element('.react-datepicker__year-select').element('[value="1994"]').click()
     browser.element('.react-datepicker__day--028').click()
+    browser.element('#subjectsInput').send_keys('History').press_enter()
     browser.element('.custom-control [for=hobbies-checkbox-1]').click()
-    browser.element('#uploadPicture').send_keys(os.getcwd()+'/1.jpg')
+    browser.element('#uploadPicture').send_keys(os.getcwd() + '/1.jpg')
     browser.element('#currentAddress').type('Perm')
     browser.element('#react-select-3-input').type('Uttar').press_enter()
     browser.element('#react-select-4-input').type('Agra').press_enter()
     browser.element('#submit').press_enter()
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    browser.all('tbody tr').should(have.exact_texts('Student Name Nikita Troshenko'))
-    browser.all('tbody tr').should(have.exact_text('Student Email gferrerewrrn@mail.ru'))
-    browser.all('tbody tr').should(have.exact_text('Gender Male'))
-    browser.all('tbody tr').should(have.exact_text('Mobile 79767658977'))
-    browser.all('tbody tr').should(have.exact_text('Date of Birth 28 June,1994'))
-    browser.all('tbody tr').should(have.exact_text('Hobbies Sports'))
-    browser.all('tbody tr').should(have.exact_text('Picture 1.jpg'))
-    browser.all('tbody tr').should(have.exact_text('Address Perm'))
-    browser.all('tbody tr').should(have.exact_text('State and City Uttar Pradesh Agra'))
+    browser.all('tbody tr') \
+        .should(have.exact_texts('Student Name Nikita Troshenko', 'Student Email gferrerewrrn@mail.ru', 'Gender Male',
+                                 'Mobile 9638460094', 'Date of Birth 28 June,1994',
+                                 'Subjects History', 'Hobbies Sports', 'Picture 1.jpg',
+                                 'Address Perm', 'State and City Uttar Pradesh Agra'))
     browser.element('#closeLargeModal').click()
