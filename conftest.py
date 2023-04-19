f6f42d0b7eee9fceaf4d8e43e5e1b077dc36a59d
Troschenko Nikita <l0.trafalgar@yandex.ru>
2023-04-19 12:00:46 +0500

    Домашнее задание №5

--- conftest.py
+++ conftest.py
@@ -9,5 +9,3 @@ def browser_management():
     browser.config.window_width = 1920
     browser.config.base_url = 'https://demoqa.com'
 
-    # browser.driver.set_window_size(1980, 1080)
-    # browser.execute_script("setTimeout(function(){debugger;}, 5000)")
